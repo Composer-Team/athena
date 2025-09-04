63508711856c63213819181b4ea284eda3cd6516
Mason <maao666@icloud.com>
2025-09-04 18:11:19 -0400

    u

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,15 @@ const config = {
   url: 'https://composer-team.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pytfhe/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'composer-team', // Usually your GitHub org/user name.
+  projectName: 'pytfhe', // Usually your repo name.
+
+  deploymentBranch: 'main',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
